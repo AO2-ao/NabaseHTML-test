f741c7f15b48efe38e112cc361abb23942beba74
Tomooki1552 <h-tomoki@tsuyama.kosen-ac.jp>
2023-09-23 22:58:02 +0900

    script.jsをサンプルの通りにした

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const Peer = window.Peer;
 
 (async function main() {
+  const localVideo = document.getElementById('js-local-stream');
+  const joinTrigger = document.getElementById('js-join-trigger');
+  const leaveTrigger = document.getElementById('js-leave-trigger');
+  const remoteVideos = document.getElementById('js-remote-streams');
+  const roomId = document.getElementById('js-room-id');
+  const roomMode = document.getElementById('js-room-mode');
+  const localText = document.getElementById('js-local-text');
+  const sendTrigger = document.getElementById('js-send-trigger');
+  const messages = document.getElementById('js-messages');
+  const meta = document.getElementById('js-meta');
+  const sdkSrc = document.querySelector('script[src*=skyway]');
+
+  meta.innerText = `
+    UA: ${navigator.userAgent}
+    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
+  `.trim();
+
+  const getRoomModeByHash = () => (location.hash === '#sfu' ? 'sfu' : 'mesh');
+
+  roomMode.textContent = getRoomModeByHash();
+  window.addEventListener(
+    'hashchange',
+    () => (roomMode.textContent = getRoomModeByHash())
+  );
+
+  const localStream = await navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true,
+    })
+    .catch(console.error);
+
+  // Render local stream
+  localVideo.muted = true;
+  localVideo.srcObject = localStream;
+  localVideo.playsInline = true;
+  await localVideo.play().catch(console.error);
 
   // eslint-disable-next-line require-atomic-updates
   const peer = (window.peer = new Peer({
@@ -8,21 +45,25 @@ const Peer = window.Peer;
     debug: 3,
   }));
 
-  if (!peer.open) {
-    return;
-  }
+  // Register join handler
+  joinTrigger.addEventListener('click', () => {
+    // Note that you need to ensure the peer has connected to signaling server
+    // before using methods of peer instance.
+    if (!peer.open) {
+      return;
+    }
 
-  const room = peer.joinRoom(roomId.value, {
-    mode: 'sfu',
-    stream: localStream,
-  });
+    const room = peer.joinRoom(roomId.value, {
+      mode: getRoomModeByHash(),
+      stream: localStream,
+    });
 
-  room.once('open', () => {
-          messages.textContent += '=== You joined ===\n';
-  });
-  room.on('peerJoin', peerId => {
-          messages.textContent += `=== ${peerId} joined ===\n`;
-  });
+    room.once('open', () => {
+      messages.textContent += '=== You joined ===\n';
+    });
+    room.on('peerJoin', peerId => {
+      messages.textContent += `=== ${peerId} joined ===\n`;
+    });
 
     // Render remote stream for new peer join in the room
    /* room.on('stream', async stream => {
@@ -35,10 +76,11 @@ const Peer = window.Peer;
       await newVideo.play().catch(console.error);
     });*/
 
-  room.on('data', ({ data, src }) => {
-    // Show a message sent to the room and who sent
-          messages.textContent += `${src}: ${data}\n`;
-  });
+    room.on('data', ({ data, src }) => {
+      // Show a message sent to the room and who sent
+      messages.textContent += `${src}: ${data}\n`;
+    });
+
     // for closing room members
     room.on('peerLeave', peerId => {
       const remoteVideo = remoteVideos.querySelector(
@@ -72,6 +114,7 @@ const Peer = window.Peer;
       messages.textContent += `${peer.id}: ${localText.value}\n`;
       localText.value = '';
     }
+  });
 
   peer.on('error', console.error);
 })();
