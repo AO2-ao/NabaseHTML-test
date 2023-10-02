2b20239c19bbb73886a0173bb994a5c196f6977f
Tomooki1552 <h-tomoki@tsuyama.kosen-ac.jp>
2023-10-02 11:55:05 +0900

    remotovideoを出力したい！

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 
+const Peer=window.Peer;
 (async function main() {
-  const localVideo = document.getElementById('js-local-stream');
+  const remoteVideo = document.getElementById('remote-video');
   const meta = document.getElementById('js-meta');
   const sdkSrc = document.querySelector('script[src*=skyway]');
-
-  const peer = new Peer( {
+  const joinTrigger = document.getElementById('js-join-trigger');
+  const localVideo = document.getElementById('js-local-video');
+  //peer初期化、webページが呼び出されたらすぐに初期化する
+  const peer =(window.Peer= new Peer( {
     key:   "f1b2a635-fca4-4150-8104-d54dfeaec4bd",
     debug: 3,
-  });
+  }));
 
   peer.on("open", (id) => {
     console.log(id);
   });
+
   meta.innerText = `
     UA: ${navigator.userAgent}
     SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
   `.trim();
-
+  //送信するmediaStreamを取得する
+  //※開発時間無いから既定のカメラとマイクを使用
   const localStream = await navigator.mediaDevices
     .getUserMedia({
       audio: true,
@@ -35,10 +40,41 @@
       console.error(`${err.name}: ${err.message}`);
     });
   
-
+  //※一旦確認用で表示させる
   // Render local stream
   localVideo.muted = true;
   localVideo.srcObject = localStream;
   localVideo.playsInline = true;
   await localVideo.play().catch(console.error);
+
+  let room;
+  joinTrigger.addEventListener('click', () => {
+      room = peer.joinRoom("hogefuga", {
+      mode: 'sfu',
+      stream: localStream,
+    });
+  })
+  room.on("open", () => {
+    console.log("Room open");
+  });
+  //各peerのvideoを格納する連想配列
+  const videoTrackMap={};
+  room.on("stream", async stream => {
+    console.log(stream.peerid);
+    videoTrackMap[stream.peerid]=stream.getVideoTracks()[0];
+
+    //試しに表示する
+    const videoTrack= videoTrackMap[stream.peerid];
+    const newRemoteMediaStream=new MediaStream();
+    if (videoTrack) {
+      newRemoteMediaStream.addTrack(videoTrack);
+  } else {
+      console.log("指定したビデオトラックが見つかりません。");
+  }
+      const newVideo = document.createElement('video');
+      newVideo.srcObject = newRemoteMediaStream;
+      newVideo.playsInline = true;
+      remoteVideo.append(newVideo);
+      await newVideo.play().catch(console.error);
+  });
 })();
